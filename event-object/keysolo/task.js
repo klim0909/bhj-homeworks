039aa53bbbc47c45939e17be1e20b4f711b45ea5
klim0909 <121249743+klim0909@users.noreply.github.com>
2023-10-08 15:56:51 +0300

    Update task.js

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,14 +17,13 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
+    document.addEventListener('keydown', (event) => {
+         if (this.currentSymbol.textContent == event.key) {
+           this.success();
+         } else {
+           this.fail();
+         }
+       });
   }
 
   success() {
