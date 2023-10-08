f4e35b5be681245ba7bd2613697b5c04bd47183a
klim0909 <121249743+klim0909@users.noreply.github.com>
2023-10-08 22:14:12 +0300

    Update task.js

---
+++ event-object/tabs/task.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const tabs = document.querySelectorAll(".tab");
+const tabContents = document.querySelectorAll(".tab__content");
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+
+    tabs.forEach(t => t.classList.remove("tab_active"));
+    tabContents.forEach(c => c.classList.remove("tab__content_active"));
+
+    tab.classList.add("tab_active");
+    tabContents[index].classList.add("tab__content_active");
+
+  });
+});
