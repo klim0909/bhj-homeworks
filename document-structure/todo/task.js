bb18f8ed51c5c210e2728e78b1530e44bcb40f3b
klim <klim.volchenkov.04@bk.ru>
2023-11-05 14:59:01 +0300

    NEW COOM

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,42 +1,44 @@
-    const taskInput = document.getElementById("task__input");
-  const tasksAdd = document.getElementById("tasks__add");
-  
-  tasksAdd.addEventListener("click", function(event) {
+const taskInput = document.getElementById("task__input");
+const tasksAdd = document.getElementById("tasks__add");
+
+tasksAdd.addEventListener("click", function(event) {
   event.preventDefault();
-  listItem()
-  });
-  function listItem() {
   
-    const tasksList = document.getElementById("tasks__list");
-  
-    const task = document.createElement("div");
-  
-    const taskTitle = document.createElement("div");
-  
-    const taskRemove = document.createElement("div");
-    taskRemove.className = "task__remove";
-  
-    const text = document.createTextNode(taskInput.value);
-  
-    if (text.nodeValue.trim() === "") {
-      return; 
-    }
-  
-    task.appendChild(taskTitle);
-    task.appendChild(taskRemove);
-  
-    taskTitle.appendChild(text);
-  
-    tasksList.appendChild(task);
-  
-    taskInput.value = "";
-  
-    // Добавляем символ крестика
-    // У меня не работало &times
-    taskRemove.textContent = "✖";
-  
-    taskRemove.addEventListener("click", function() {
-      tasksList.removeChild(task);
-    });
-  
+  listItem();
+});
+
+function listItem() {
+
+  const tasksList = document.getElementById("tasks__list");
+
+  const task = document.createElement("div");
+  task.classList.add("task");
+
+  const taskTitle = document.createElement("div");
+  taskTitle.classList.add("task__title");
+
+  const taskRemove = document.createElement("a");
+  taskRemove.href = "#";
+  taskRemove.classList.add("task__remove");
+  taskRemove.textContent = "&times;";
+
+  const text = document.createTextNode(taskInput.value);
+
+  if (text.nodeValue.trim() === "") {
+    return;
   }
+
+  task.appendChild(taskTitle);
+  task.appendChild(taskRemove);
+
+  taskTitle.appendChild(text);
+
+  tasksList.appendChild(task);
+
+  taskInput.value = "";
+
+  taskRemove.addEventListener("click", function() {
+    tasksList.removeChild(task); 
+  });
+
+}
